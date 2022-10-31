94288d809933ef22fa7515f221f293e4b1928802
Christophe Flaus <christophe.flaus@insee.fr>
2022-10-31 17:18:12 +0100

    création classe NomenclatureValueProviderImpl

---
+++ src/main/java/com/protools/flowableDemo/services/providers/ProviderRestTemplate.java
@@ -0,0 +1,20 @@
+package com.protools.flowableDemo.services.providers;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class ProviderRestTemplate extends RestTemplate {
+    public ProviderRestTemplate() {
+        super();
+
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(List.of(MediaType.TEXT_PLAIN));
+
+        this.getMessageConverters().add(0, converter);
+    }
+}
