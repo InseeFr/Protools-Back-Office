c2a440264655be8b47dd45434f4b07fb2eb8caf9
Christophe Flaus <christophe.flaus@insee.fr>
2022-11-02 10:09:13 +0100

    QuestionnaireModelValueProviderImpl : refactoring et ajout test unitaire

--- src/main/java/com/protools/flowableDemo/services/providers/QuestionnaireModelValueProviderImpl.java
+++ src/main/java/com/protools/flowableDemo/services/providers/QuestionnaireModelValueProviderImpl.java
@@ -1,13 +1,11 @@
 package com.protools.flowableDemo.services.providers;
 
 import com.protools.flowableDemo.enums.CollectionPlatform;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 import java.util.Map;
 
 @Service
@@ -16,18 +14,15 @@ public class QuestionnaireModelValueProviderImpl implements QuestionnaireModelVa
     @Value("${fr.insee.questionnaire.model.value.provider.uri:#{null}}")
     private String questionnaireModelValueProviderUri;
 
+    @Autowired
+    private ProviderRestTemplate restTemplate;
+
     @Override
     public Map<?, ?> getQuestionnaireModelValue(CollectionPlatform platform, String questionnaireModelId)
             throws Exception {
         String uri = questionnaireModelValueProviderUri + "/" + getPath(platform, questionnaireModelId);
 
         HttpEntity<String> request = new HttpEntity<>(new HttpHeaders());
-
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setSupportedMediaTypes(List.of(MediaType.TEXT_PLAIN));
-
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(0, converter);
 
         ResponseEntity<Map> response = restTemplate.exchange(uri, HttpMethod.GET, request, Map.class);
 
---
+++ src/test/java/com/protools/flowableDemo/services/providers/QuestionnaireModelValueProviderImplTest.java
@@ -0,0 +1,51 @@
+package com.protools.flowableDemo.services.providers;
+
+import com.protools.flowableDemo.enums.CollectionPlatform;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.*;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+public class QuestionnaireModelValueProviderImplTest {
+    @Mock
+    private ProviderRestTemplate restTemplate;
+
+    @InjectMocks
+    private QuestionnaireModelValueProviderImpl provider = new QuestionnaireModelValueProviderImpl();
+
+    @Test
+    public void givenMockingIsDoneByMockito_whenExchangeIsCalled_shouldReturnMockedObject() throws Exception {
+        String questionnaireModelValueProviderUri = "https://questionnaire-model-provider/QuestionnaireModels";
+
+        Field field = provider.getClass().getDeclaredField("questionnaireModelValueProviderUri");
+        field.setAccessible(true);
+        field.set(provider, questionnaireModelValueProviderUri);
+
+        CollectionPlatform platform = CollectionPlatform.COLEMAN;
+
+        String questionnaireModelId = "model_1";
+
+        String uri = questionnaireModelValueProviderUri + "/coleman/" + questionnaireModelId + ".json";
+
+        HttpEntity<String> request = new HttpEntity<>(new HttpHeaders());
+
+        Map<?, ?> questionnaireModelValue = Map.of("id", "id_1", "model", "model_1");
+
+        Mockito
+                .when(restTemplate.exchange(uri, HttpMethod.GET, request, Map.class))
+                .thenReturn(new ResponseEntity<>(questionnaireModelValue, HttpStatus.OK));
+
+        Map<?, ?> providedQuestionnaireModelValue = provider.getQuestionnaireModelValue(platform, questionnaireModelId);
+
+        Assertions.assertEquals(questionnaireModelValue, providedQuestionnaireModelValue);
+    }
+}
+
