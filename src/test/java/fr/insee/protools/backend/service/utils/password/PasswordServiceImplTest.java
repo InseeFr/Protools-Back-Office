13667872105b1bc7ce2d094b55933dff2b980c2c
SNDIL - Marc <marc.berger@insee.fr>
2024-09-27 11:32:03 +0200

    tests: fix un test pour éviter qu'il charge le contexte spring

--- src/test/java/fr/insee/protools/backend/service/utils/password/PasswordServiceImplTest.java
+++ src/test/java/fr/insee/protools/backend/service/utils/password/PasswordServiceImplTest.java
@@ -1,36 +1,47 @@
 package fr.insee.protools.backend.service.utils.password;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.passay.PasswordData;
 import org.passay.PasswordValidator;
 import org.passay.RuleResult;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(classes = PasswordServiceImpl.class)
 @TestPropertySource(properties = {
         "fr.insee.protools.password.create.withDigits = true",
         "fr.insee.protools.password.create.withUpperCase = true",
         "fr.insee.protools.password.create.withLowerCase = true",
         "fr.insee.protools.password.create.withSpecial = true",
 })
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(classes = PasswordServiceImpl.class) //For autowire
+//These 2 are needed to inject @Values
+@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 class PasswordServiceImplTest {
 
     @Autowired
-    private PasswordService passwordService;
+    private PasswordServiceImpl passwordService;
+
 
     @Test
     void testGeneratePassword() {
         //Prepare
-        var rules = ((PasswordServiceImpl)passwordService).generateRandomPasswordCharacterRules(true,true,true,true);
+        var rules = ((PasswordServiceImpl) passwordService).generateRandomPasswordCharacterRules(true, true, true, true);
         PasswordValidator passwordValidator = new PasswordValidator(rules);
 
         //Generate and validate X1000
-        for (int i = 0; i < 1000 ; i++) {
-            for(int length=8; length < 19; length ++) { // and for various sizes
+        for (int i = 0; i < 1000; i++) {
+            for (int length = 8; length < 19; length++) { // and for various sizes
                 //Execute method under test
                 String generatePassword = passwordService.generatePassword(length);
 
